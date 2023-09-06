7e149407199d335c563b113effe2d1cfb8d70de4
Anastasia Ageeva <anastasia.smirnova@gmail.com>
2023-09-06 22:41:05 +0300

    added seminar 5

--- seminar_project/blogapp/admin.py
+++ seminar_project/blogapp/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    """Автор"""
+    list_display = ['name', 'lastname', 'email']
+    list_filter = ['birthdate', 'lastname']
+    search_fields = ['name', 'lastname', 'bio']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+
+    """Отдельный продукт."""
+    # fields = ['name', 'description', 'category', 'date_added', 'rating']
+    readonly_fields = ['birthdate']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name', 'lastname'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Подробное описание автора',
+                'fields': ['bio', 'birthdate'],
+            },
+        ),
+    ]
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Пост"""
+    list_display = ['title', 'author']
+    search_fields = ['title', 'author', 'content']
+    search_help_text = 'Поиск по полю название, автор, контент'
+
+    """Отдельный продукт."""
+    # fields = ['name', 'description', 'category', 'date_added', 'rating']
+    readonly_fields = ['public_date']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Подробное описание статьи',
+                'fields': ['content', 'category', 'views'],
+            },
+        ),
+    ]
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
